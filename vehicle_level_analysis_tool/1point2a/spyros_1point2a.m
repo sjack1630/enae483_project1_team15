590e650f164c9d67de4d02018c4e5d5557a3a0e2
smazis <smazis@terpmail.umd.edu>
2025-09-16 19:06:30 -0400

    1.2a and 1.3a reworked

--- vehicle_level_analysis_tool/1point2a/spyros_1point2a.m
+++ vehicle_level_analysis_tool/1point2a/spyros_1point2a.m
@@ -1,26 +1,23 @@
-% LOX/RP1 row
-% combo: first prop: LOX/LCH4, second prop: LOX/RPI
 addpath('..')
-
-% Set constants based on requirements and propellant combination
-delta1 = 0.08;
-delta2 = 0.08;
-Isp1 = 327;
-Isp2 = 311;
-m_pl = 26000;
-
-% Initialize/populate arrays
-X = 0:0.001:1;
-m_stage1_arr = [];
-m_stage2_arr = [];
+% Fill the mass arrays
+X = 0:0.01:1;
+m1_arr = [];
+m2_arr = [];
 m0_arr = [];
 
-% For each X value, call mass_function and populate the arrays for mass of
-% stage 1, mass of stage 2, and total mass
+% Constants based on the propellents chosen and requirements
+Isp1 = 327;     %LOX/LCH4
+Isp2 = 311;     %LOX/RP1
+delta1 = 0.08;
+delta2 = delta1;
+mpl = 26000;
+
+% For the X mass value call the "mass_function" and fill the arrays for mass
+% stage 1, mass stage 2, and total mass
 for i = 1:length(X)
     [m_in1, m_in2, m_pr1, m_pr2, m0] = mass_function(Isp1, Isp2, X(i), delta1, delta2);
-    m_stage1_arr(end+1) = m_in1 + m_pr1;
-    m_stage2_arr(end+1) = m_in2 + m_pr2;
+    m1_arr(end+1) = m_in1 + m_pr1;
+    m2_arr(end+1) = m_in2 + m_pr2;
     m0_arr(end+1) = m0;
 end
 
@@ -28,21 +25,21 @@ end
 % the associated X for the minimum gross mass
 min_m0 = realmax;
 for i = 1:length(m0_arr)
-    if m_stage1_arr(i) > 0 && m_stage2_arr(i) > 0 && m0_arr(i) < min_m0
+    if m1_arr(i) > 0 && m2_arr(i) > 0 && m0_arr(i) < min_m0
         min_m0 = m0_arr(i);
         min_m0_X = X(i);
     end
 end
 
-% Plot mass of stage 1, mass of stage 2, and total mass in metric tons 
-% as a function of X. Also plot as a point the minimum gross mass
-plot(X, m_stage1_arr/1000)
+% Plot mass stage 1, mass stage 2, and total mass in metric tons 
+% as a function of X. Plot minimum gross mass as a point on the graph
+plot(X, m1_arr/1000, "Linewidth", 4)
 hold on
-plot(X, m_stage2_arr/1000)
-plot(X, m0_arr/1000)
-plot(min_m0_X, min_m0/1000, '.', MarkerSize=20)
+plot(X, m2_arr/1000, "Linewidth", 4)
+plot(X, m0_arr/1000, "Linewidth", 4)
+plot(min_m0_X, min_m0/1000, '*', "LineWidth", 4, MarkerSize=30)
 ylim([0, 1e4])
-legend("First Stage Mass", "Second Stage Mass", "Gross Vehicle Mass")
-xlabel("First Stage \DeltaV Fraction")
-ylabel("Mass (t)")
-
+legend("1st Stage Mass", "2nd Stage Mass", "Total Mass", "FontSize", 20, "Location", "southwest")
+xlabel("First Stage DeltaV Fraction", "FontSize", 20)
+ylabel("Mass (metric tons)", "FontSize", 20)
+title("1st Stage: LOX/LCH4, 2nd Stage: LOX/RP1", "FontSize", 20)
