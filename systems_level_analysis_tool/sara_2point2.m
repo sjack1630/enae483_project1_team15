08e8cb59bfcff195c70212b42ef18644e64387e1
s-carter04 <carter04@terpmail.umd.edu>
2025-10-05 13:06:25 -0400

    Create sara_2point2.m

---
+++ systems_level_analysis_tool/sara_2point2.m
@@ -0,0 +1,15 @@
+
+first_stage = "LH2"
+second_stage = "LH2" 
+X = 0.5;
+
+[num_engines_stage1, num_engines_stage2, stage1_only_total_mass, stage2_only_total_mass, total_mass, total_height, stage1_T_to_W, stage2_T_to_W] = get_MER_total_mass(first_stage, second_stage, X)
+
+fprintf('Number of engines (Stage 1): %.2f\n', num_engines_stage1);
+fprintf('Number of engines (Stage 2): %.2f\n', num_engines_stage2);
+fprintf('Stage 1 only total mass: %.2f kg\n', stage1_only_total_mass);
+fprintf('Stage 2 only total mass: %.2f kg\n', stage2_only_total_mass);
+fprintf('Total mass: %.2f kg\n', total_mass);
+fprintf('Total height: %.2f m\n', total_height);
+fprintf('Stage 1 Thrust-to-Weight ratio: %.3f\n', stage1_T_to_W);
+fprintf('Stage 2 Thrust-to-Weight ratio: %.3f\n', stage2_T_to_W);
